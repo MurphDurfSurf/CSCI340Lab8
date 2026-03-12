47ee65abce51cf9104e483cbfd4271c0bdae6abd
Mason Murphy <murphyma@hendrix.edu>
2026-03-11 23:12:20 -0500

    Created Minis modle

---
+++ Modles/Mini.cs
@@ -0,0 +1,22 @@
+public class Mini
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Faction { get; set; }
+
+    public int Movement { get; set; }
+
+    public int Toughness { get; set; }
+
+    public int Wounds { get; set; }
+
+    public int Save { get; set; }
+
+    public int Leadership { get; set; }
+
+    public int ObjectiveControl { get; set; }
+
+    public int Points { get; set; }
+}
